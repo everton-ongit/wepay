b2de3f6d676eaca11a44d9947038d2337acb956e
Everton Santos <everton@Evertons-MacBook-Pro.local>
2017-02-18 19:44:58 -0300

    finishapi

--- app/api/apigeneric.js
+++ app/api/apigeneric.js
@@ -1,76 +1,79 @@
-var express    = require('express');
+var express = require('express');
 var router = express.Router();
 
-var api = function(entityName, Model){
+var api = function (entityName, Model) {
 
-router.route('/'+entityName)
+    router.route('/' + entityName)
 
-//lista
-.get(function (req, res) {
-    Model.find(function (err, bears) {
-        if (err)
-            res.send(err);
-
-        res.json(bears);
-    });
-})
-//salva
-.post( function (req, res) {
-
-    var model = new Model();
-    model.nome = req.body.valor;
-    
-    console.log('body', req.body);
-
-    model.save(function (err) {
-        if (err)
-            res.send(err);
-
-        res.json({ message: 'Salvo com sucesso!' });
-    });
-});
-
-router.route('/'+ entityName +'/:id')
-
-    //retorna por id
-    .get(function(req, res) {
-        Model.findById(req.params.id, function(err, model) {
-            if (err)
-                res.send(err);
-            res.json(model);
-        });
-    })
-    //atualiza
-    .put(function(req, res) {
-
-        Model.findById(req.params.id, function(err, model) {
-
-            if (err)
-                res.send(err);
-
-            model.nome = req.body.nome;  
-
-            // salva
-            model.save(function(err) {
+        //lista
+        .get(function (req, res) {
+            Model.find(function (err, bears) {
                 if (err)
                     res.send(err);
 
-                res.json({ message: 'Atualizado com sucesso!' });
+                res.json(bears);
+            });
+        })
+        //salva
+        .post(function (req, res) {
+
+            var model = new Model();
+            for (var p in req.body) {
+                model[p] = req.body[p];
+            }
+            model.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Salvo com sucesso from apigeneric!' });
             });
         });
-    })
 
-    //deleta
-    .delete(function(req, res) {
-        Model.remove({
-            _id: req.params.id
-        }, function(err, bear) {
-            if (err)
-                res.send(err);
+    router.route('/' + entityName + '/:id')
 
-            res.json({ message: 'Deletado com sucesso!' });
+        //retorna por id
+        .get(function (req, res) {
+            Model.findById(req.params.id, function (err, model) {
+                if (err)
+                    res.send(err);
+                res.json(model);
+            });
+        })
+        //atualiza
+        .put(function (req, res) {
+
+            Model.findById(req.params.id, function (err, model) {
+
+                if (err)
+                    res.send(err);
+
+                for (var p in req.body) {
+                    model[p] = req.body[p];
+                }
+
+                // salva
+                model.save(function (err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Atualizado com sucesso!' });
+                });
+            });
+        })
+
+        //deleta
+        .delete(function (req, res) {
+            Model.remove({
+                _id: req.params.id
+            }, function (err, bear) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Deletado com sucesso!' });
+            });
         });
-    });
+
+    return router;
 }
 
 module.exports = api;
