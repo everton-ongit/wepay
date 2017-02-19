c5c3272a348d82300f7cd5f6f1a76ed64b75adf5
Everton Santos <everton@Evertons-MacBook-Pro.local>
2017-02-19 07:42:18 -0300

    calculando

--- client/app/start/start.ctrl.js
+++ client/app/start/start.ctrl.js
@@ -3,22 +3,25 @@
 	angular.module('myApp').controller('StartCtrl', ['$scope', '$rootScope', 'appService', 'transacaoService', 'entradaService', StartCtrl]);
 
 	function StartCtrl($scope, $rootScope, appService, transacaoService, entradaService) {
-		
+
 		$scope.data = [];
-    $scope.labels = ['Fevereiro'];
-    $scope.series = ['pagas'];
+		$scope.labels = ['Fevereiro'];
+		$scope.series = ['pagas'];
 
 		$scope.$parent.title = "Inicio";
-
 		$rootScope.showPaybutton = true;
 
 		$scope.list = [];
+
+		$scope.entradas = 0;
+		$scope.saidas = 0;
 
 		transacaoService.listarRecebimentos(appService.usuarioLogado._id)
 			.then(function (retorno) {
 				for (var i of retorno.data) {
 					i.tipo = 'recebimento';
 					$scope.list.push(i)
+					$scope.entradas += i.valor;
 				}
 			})
 
@@ -27,6 +30,7 @@
 				for (var i of retorno.data) {
 					i.tipo = 'pagamento';
 					$scope.list.push(i)
+					$scope.saidas += i.valor;
 				}
 			})
 
@@ -35,20 +39,21 @@
 				for (var i of retorno.data) {
 					i.tipo = 'entradas';
 					$scope.list.push(i)
+					$scope.entradas += i.valor;
 				}
 			})
 
 
- 		ajustarChart();
-		
+		ajustarChart();
+
 		function ajustarChart() {
 			var soma = 0;
-			angular.forEach($scope.list, function(value, key){
-        if(value.valor){
-        	soma += value.valor;
-        }
-      });
-      $scope.data.push(soma);
+			angular.forEach($scope.list, function (value, key) {
+				if (value.valor) {
+					soma += value.valor;
+				}
+			});
+			$scope.data.push(soma);
 		}
 
 	}
