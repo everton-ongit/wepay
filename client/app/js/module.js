fd8f97388a9eb76f39659259a5334d026e655d06
Tullyo Ryann <Tullyo Ryann>
2017-02-19 07:00:03 -0300

    bower

--- client/app/js/module.js
+++ client/app/js/module.js
@@ -1,2 +1,2 @@
 'use strict';
-angular.module('myApp', ['ngMaterial', 'ui.router', 'ui.utils.masks']);
+angular.module('myApp', ['ngMaterial', 'ui.router', 'ui.utils.masks', 'chart.js']);
--- client/app/start/start.ctrl.js
+++ client/app/start/start.ctrl.js
@@ -3,11 +3,27 @@
 	angular.module('myApp').controller('StartCtrl', ['$scope', '$rootScope', 'transacaoService', StartCtrl]);
 
 	function StartCtrl($scope, $rootScope, transacaoService) {
+		
+		$scope.data = [];
+    $scope.labels = ['Fevereiro'];
+    $scope.series = ['pagas'];
 		$scope.$parent.title = "Inicio";
 
 		$rootScope.showPaybutton = true;
 
 		$scope.list = transacaoService.listar();
+
+ 		ajustarChart();
+		
+		function ajustarChart() {
+			var soma = 0;
+			angular.forEach($scope.list, function(value, key){
+        if(value.valor){
+        	soma += value.valor;
+        }
+      });
+      $scope.data.push(soma);
+		}
 
 	}
 
