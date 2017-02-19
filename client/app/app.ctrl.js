98e2613c73af5e771bcd1255816eb12022f8bd23
Everton Santos <everton@Evertons-MacBook-Pro.local>
2017-02-19 03:14:49 -0300

    login

--- client/app/app.ctrl.js
+++ client/app/app.ctrl.js
@@ -1,14 +1,39 @@
 (function () {
     'use strict';
-    angular.module('myApp').controller('AppCtrl', ['$scope', 'appService', '$state', '$mdSidenav', AppCtrl]);
+    angular.module('myApp').controller('AppCtrl', ['$scope', 'appService', '$state', '$mdSidenav', '$mdDialog', AppCtrl]);
 
-    function AppCtrl($scope, appService, $state, $mdSidenav) {
+    function AppCtrl($scope, appService, $state, $mdSidenav, $mdDialog) {
 
         $scope.title = appService.title;
 
         $scope.toggleSidenav = function (menuId) {
             $mdSidenav(menuId).toggle();
         };
+
+        $scope.isUsuarioLogado = function(){
+            return appService.usuarioLogado._id;
+        } 
+
+        $scope.logar = function(){
+            
+            if(!appService.usuarioLogado._id){
+
+                $mdDialog.show({
+                    controller: 'LoginCtrl',
+                    templateUrl: 'login/login.html',
+                    parent: angular.element(document.body)
+                })
+                .then(function(){
+                    $scope.logar();
+                })
+            }
+            else{
+                $scope.usuarioLogado = appService.usuarioLogado;
+                $state.go('home.start');
+            }
+        }
+
+        $scope.logar();
 
         $scope.menu = [
             {
--- client/app/login/login.ctrl.js
+++ client/app/login/login.ctrl.js
@@ -1,10 +1,33 @@
-(function(){
-'use strict';
-	angular.module('myApp').controller('LoginCtrl',['$scope', LoginCtrl]);
+(function () {
+	'use strict';
+	angular.module('myApp').controller('LoginCtrl', ['$scope', 'usuarioService', 'appService', '$mdDialog', '$mdToast', LoginCtrl]);
 
-	function LoginCtrl($scope){
-		
-	
+	function LoginCtrl($scope, usuarioService, appService, $mdDialog, $mdToast) {
+
+		$scope.usuarios = usuarioService.listar();
+
+		$scope.usuario = {};
+
+		$scope.login = function () {
+			var u = $scope.usuarios.filter(function (m) {
+				return m.nome == $scope.usuario.nome && m.senha == $scope.usuario.senha;
+			})[0];
+
+			if (u) {
+				appService.usuarioLogado = u;
+				$mdDialog.hide();
+			}
+		}
+
+		$scope.cadastar = function () {
+			usuarioService.salvar($scope.usuario);
+			$mdToast.show(
+				$mdToast.simple()
+					.textContent('Usuario cadastro com sucesso! Efetue o login')
+					.hideDelay(3000)
+			);
+		}
+
 	}
 
 })();
